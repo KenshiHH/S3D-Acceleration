1153bb2139de98dd336c1593f09665933b336cbc
Kenshi <kenshihh@gmx.net>
2017-12-09 08:00:58 +0100

    added check if file is already patched and if there i a valid file selected

--- GCode_Edit/Form1.cs
+++ GCode_Edit/Form1.cs
@@ -13,9 +13,10 @@ namespace GCode_Edit
 {
     public partial class Form1 : Form
     {
-        string GCodeFilePath;
+        string GCodeFilePath =null;
         string GCodePatchedFilePath;
-
+        string tempPath;
+        string patchString = "_patched";
         public Form1()
         {
             InitializeComponent();
@@ -54,9 +55,16 @@ namespace GCode_Edit
                     {
                         using (myStream)
                         {
-                            GCodeFilePath = openFileDialog1.FileName;
-                            label_FileSource.Text = Path.GetFileNameWithoutExtension(GCodeFilePath);
-                            GCodePatchedFilePath = Path.GetDirectoryName(GCodeFilePath) +"\\"+ Path.GetFileNameWithoutExtension(GCodeFilePath) + "_patched" + Path.GetExtension(GCodeFilePath);
+                            tempPath = openFileDialog1.FileName;
+                            if (tempPath.Contains(patchString)){
+                                GCodeFilePath = null;
+                                MessageBox.Show("Error: File already patched!?!");
+                            }
+                            else {
+                                GCodeFilePath = openFileDialog1.FileName;
+                                label_FileSource.Text = Path.GetFileNameWithoutExtension(GCodeFilePath);
+                                GCodePatchedFilePath = Path.GetDirectoryName(GCodeFilePath) + "\\" + Path.GetFileNameWithoutExtension(GCodeFilePath) + "_patched" + Path.GetExtension(GCodeFilePath);
+                            }
                         }
                     }
                 }
@@ -69,61 +77,68 @@ namespace GCode_Edit
 
         private void button2_Click(object sender, EventArgs e)
         {
-            StreamReader gcode = new StreamReader(GCodeFilePath);
-            StreamWriter gcode2 = new StreamWriter(GCodePatchedFilePath);
-            string line;
-            string newSkirtACC = "M204 S" + acceleration_Skirt.Value.ToString();
-            string newOuterACC = "M204 S" + acceleration_OuterPerimeter.Value.ToString();
-            string newInnerACC = "M204 S" + acceleration_InnerPerimeter.Value.ToString();
-            string newSolidACC = "M204 S" + acceleration_SolidLayer.Value.ToString();
-            string newInfillACC = "M204 S" + acceleration_Infill.Value.ToString();
-            string newGapACC = "M204 S" + acceleration_GapFill.Value.ToString();
-            string newSupportACC = "M204 S" + acceleration_Support.Value.ToString();
-            string newDenseSupportACC = "M204 S" + acceleration_DenseSupport.Value.ToString();
-            int linesfound = 0;
-
-            while ((line = gcode.ReadLine()) != null)
+            if (GCodeFilePath == null)
             {
-                gcode2.WriteLine(line);
-                switch (line)
+                MessageBox.Show("Error: no valid file selected.");
+            }
+            else
+            {
+                StreamReader gcode = new StreamReader(GCodeFilePath);
+                StreamWriter gcode2 = new StreamWriter(GCodePatchedFilePath);
+                string line;
+                string newSkirtACC = "M204 S" + acceleration_Skirt.Value.ToString();
+                string newOuterACC = "M204 S" + acceleration_OuterPerimeter.Value.ToString();
+                string newInnerACC = "M204 S" + acceleration_InnerPerimeter.Value.ToString();
+                string newSolidACC = "M204 S" + acceleration_SolidLayer.Value.ToString();
+                string newInfillACC = "M204 S" + acceleration_Infill.Value.ToString();
+                string newGapACC = "M204 S" + acceleration_GapFill.Value.ToString();
+                string newSupportACC = "M204 S" + acceleration_Support.Value.ToString();
+                string newDenseSupportACC = "M204 S" + acceleration_DenseSupport.Value.ToString();
+                int linesfound = 0;
+
+                while ((line = gcode.ReadLine()) != null)
                 {
-                    case "; outer perimeter":
-                        linesfound++;
-                        gcode2.WriteLine(newOuterACC);
-                        break;
-                    case "; inner perimeter":
-                        linesfound++;
-                        gcode2.WriteLine(newInnerACC);
-                        break;
-                    case "; skirt":
-                        linesfound++;
-                        gcode2.WriteLine(newSkirtACC);
-                        break;
-                    case "; solid layer":
-                        linesfound++;
-                        gcode2.WriteLine(newSolidACC);
-                        break;
-                    case "; infill":
-                        linesfound++;
-                        gcode2.WriteLine(newInfillACC);
-                        break;
-                    case "; gap fill":
-                        linesfound++;
-                        gcode2.WriteLine(newGapACC);
-                        break;
-                    case "; support":
-                        linesfound++;
-                        gcode2.WriteLine(newSupportACC);
-                        break;
-                    case "; dense support":
-                        linesfound++;
-                        gcode2.WriteLine(newDenseSupportACC);
-                        break;
+                    gcode2.WriteLine(line);
+                    switch (line)
+                    {
+                        case "; outer perimeter":
+                            linesfound++;
+                            gcode2.WriteLine(newOuterACC);
+                            break;
+                        case "; inner perimeter":
+                            linesfound++;
+                            gcode2.WriteLine(newInnerACC);
+                            break;
+                        case "; skirt":
+                            linesfound++;
+                            gcode2.WriteLine(newSkirtACC);
+                            break;
+                        case "; solid layer":
+                            linesfound++;
+                            gcode2.WriteLine(newSolidACC);
+                            break;
+                        case "; infill":
+                            linesfound++;
+                            gcode2.WriteLine(newInfillACC);
+                            break;
+                        case "; gap fill":
+                            linesfound++;
+                            gcode2.WriteLine(newGapACC);
+                            break;
+                        case "; support":
+                            linesfound++;
+                            gcode2.WriteLine(newSupportACC);
+                            break;
+                        case "; dense support":
+                            linesfound++;
+                            gcode2.WriteLine(newDenseSupportACC);
+                            break;
+                    }
                 }
-             }
-            gcode2.Close();
-            //MessageBox.Show("Lines found: " + linesfound.ToString());
-            MessageBox.Show("lines added: "+ linesfound.ToString());
+                gcode2.Close();
+                //MessageBox.Show("Lines found: " + linesfound.ToString());
+                MessageBox.Show("lines added: " + linesfound.ToString());
+            }
         }
 
         private void textBox_OuterAcceleration_TextChanged(object sender, EventArgs e)
